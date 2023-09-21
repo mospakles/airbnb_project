5d438fa842d342cc20763e36cd5328dcd6725e83
unknown <motunrayodusina@gmail.com>
2023-09-21 07:25:00 +0100

    air bnb

--- src/app/api/reservations/route.ts
+++ src/app/api/reservations/route.ts
@@ -22,7 +22,7 @@ export async function POST(request: Request) {
       id: listingId,
     },
     data: {
-      reservations: {
+      reservation: {
         create: {
           userId: currentUser.id,
           startDate,
